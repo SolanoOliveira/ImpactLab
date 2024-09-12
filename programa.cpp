506893c14e1e8b67e7383444f13ce9394a0bd65e
Solano <solano.oliveira@icomp.ufam.edu.br>
2024-09-11 20:00:53 -0400

    lgbt pride flag

--- programa.cpp
+++ programa.cpp
@@ -7,7 +7,9 @@ int main() {
     ofstream image;
     image.open("giraffe_p3.ppm");
 
-    int cor = 255;
+    int R = 255;
+    int G = 0;
+    int B = 0;
 
     if (image.is_open()) {
         image << "P3" << endl;         
@@ -17,11 +19,32 @@ int main() {
 
     //across the image
     for(int y = 0; y<250; y++){
-        if(y > 125){
-           cor = 0; 
+        if(y == 41){
+           R = 255;
+           G = 140;
+           B = 0; 
+        }
+        else if(y == 82){
+           R = 255;
+           G = 237;
+           B = 0; 
+        }else if(y == 123){
+             R = 0;
+           G = 128;
+           B = 38; 
+        }
+        else if(y == 164){
+             R = 0;
+           G = 76;
+           B = 255; 
+        }
+         else if(y == 205){
+             R = 115;
+           G = 41;
+           B = 130; 
         }
      for(int x = 0; x<250; x++){
-        image << cor <<  " " << cor << " " << cor << endl;
+        image << R <<  " " << G << " " << B << endl;
     }   
     }
 
